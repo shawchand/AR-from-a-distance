73e75e90c67e1cdb7e0917f11dbc6a7a1bd10a1a
shawchand <34025914+shawchand@users.noreply.github.com>
2020-04-09 11:49:39 +0200

    drawing trial

---
+++ Advanced AR from a distance/Assets/Drawing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Texture2D texture = new Texture2D(128, 128);
+        GetComponent<Renderer>().material.mainTexture = texture;
+
+        for(int y = 0; y < texture.height; y++)
+        {
+            for (int x = 0;  x < texture.width; x++)
+            {
+                Color color = ((x & y) != 0 ? Color.white : Color.gray); //what does this do?
+                texture.SetPixel(x, y, color);
+            }
+        }
+        texture.Apply();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Advanced AR from a distance/Assets/Scripts/followCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class followCamera : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = Input.mousePosition;
+        temp.z = 10f;
+        this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+    }
+} 
